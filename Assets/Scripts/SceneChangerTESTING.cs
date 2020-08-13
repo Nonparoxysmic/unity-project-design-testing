4252b6d19bfaae301bc62c6d7601fd56c90ae8ee
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2020-08-12 22:23:13 -0500

    Getting started
    Added some scenes and started testing scene transitions

---
+++ Assets/Scripts/SceneChangerTESTING.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangerTESTING : MonoBehaviour
+{
+    public string titleScreenName;
+    public string mainMenuName;
+    public float defaultTimeLeft;
+
+    private float timeLeft;
+    private string nextScene;
+
+    void Start()
+    {
+        timeLeft = defaultTimeLeft;
+        nextScene = titleScreenName;
+    }
+
+    void Update()
+    {
+        if (nextScene == titleScreenName)
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                SceneManager.LoadSceneAsync(nextScene, LoadSceneMode.Additive);
+                timeLeft = defaultTimeLeft;
+                nextScene = mainMenuName;
+            }
+        }
+        else if (nextScene == mainMenuName)
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                SceneManager.LoadSceneAsync(nextScene, LoadSceneMode.Additive);
+                SceneManager.UnloadSceneAsync(titleScreenName);
+                nextScene = "None";
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # unity-project-design-testing
  I'm learning how to design Unity projects.
+ Made with Unity 2018.4.25f1
